6b658e8973d4d99849ea27b38f1a4ab747f390e1
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-06-04 11:58:32 -0400

    redux added

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,6 @@
-import {ThemeContext} from '@emotion/react';
 import { makeStyles } from '@mui/styles';
+
+const drawerWidth =240;
 
 export default makeStyles((theme) => ({
 toolbar: {
@@ -16,6 +17,21 @@ menuButton: {
   marginRight: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     display: 'none',
-  }
-}
+  },
+},
+drawer: {
+  [theme.breakpoints.up('sm')]: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+},
+drawerPaper: {
+  width: drawerWidth,
+},
+linkButton: {
+  '&:hover' : {
+    color: 'white !important',
+    textDecoration: 'none',
+  },
+},
 }));
