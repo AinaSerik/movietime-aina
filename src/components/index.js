3596c83e1bff579688030c1a72da1a336dfb84ab
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-06-06 13:28:09 -0400

    search movie by genres

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import Search from './Search/Search';
+
 export { default as Actors } from './Actors/Actors';
 export { default as MovieInformation } from './MovieInformation/MovieInformation';
 export { default as Movies } from './Movies/Movies';
@@ -5,4 +7,5 @@ export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as MovieList } from './MovieList/MovieList';
-export {default as Movie} from './Movie/Movie';
+export { default as Movie } from './Movie/Movie';
+export { default as Search } from './Search/Search';
